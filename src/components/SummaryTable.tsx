41c7353e32df65c419c70c52b01a41d835eddfd5
Eduardo Schwanke <carvalhoe089@gmail.com>
2023-01-27 08:57:39 -0300

    front-end completo

---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,40 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+import { HabitDay } from "./HabitDay";
+
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize = 18 * 7
+const amountOfDaysToFill =minimumSummaryDatesSize - summaryDates.length
+
+export function SummaryTable() {
+    return (
+        <div className="w-full flex">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
+                <div className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold">D</div>
+                <div className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold">S</div>
+                <div className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold">T</div>
+                <div className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold">Q</div>
+                <div className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold">Q</div>
+                <div className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold">S</div>
+                <div className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold">S</div>
+            </div>
+
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                {
+                    summaryDates.map((day, index) => {
+                        return (
+                            <HabitDay key={day.toString()}/>
+                        )
+                    })
+                }
+                {
+                    amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                        return (
+                            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
