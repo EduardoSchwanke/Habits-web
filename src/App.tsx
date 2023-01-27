6b75818cfe9aa744c08687a9280c8187459b16e1
Eduardo Schwanke <carvalhoe089@gmail.com>
2023-01-27 08:12:38 -0300

    Componente de Header feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from "./components/header"
+
 function App() {
 
   return (
-    <button className="border border-1 border-slate-600 px-2 py-1 m-10 rounded-lg">home</button>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header/>
+      </div>
+    </div>
   )
 }
 
