58bb205516c6c0599faafa93ef294a3ee01072b1
Eduardo Schwanke <carvalhoe089@gmail.com>
2023-01-29 10:32:47 -0300

    popover detalhe do dia completo

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+interface ProgressBarProps {
+    progress: number
+}
+
+export function ProgessBar(props: ProgressBarProps) {
+    return(
+        <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+            <div
+                role={'progressbar'}
+                aria-label="progresso de habitos completos nesse dia" 
+                aria-valuenow={props.progress}
+                className='h-3 rounded-xl bg-violet-600'
+                style={{width: `${props.progress}%`}}
+            ></div>
+        </div>
+    )
+}
